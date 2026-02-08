186a4e2334dc4f16a75ae6832d78fe15f89b3cee
wlehan <williamclehan@gmail.com>
2026-02-08 17:07:02 -0600

    Backend working with test endpoint

---
+++ backend/src/main/java/com/augustana/golf/GolfApplication.java
@@ -0,0 +1,13 @@
+package com.augustana.golf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolfApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/augustana/golf/GolfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augustana.golf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GolfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
