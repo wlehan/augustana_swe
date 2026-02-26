01267bcedfe0f74046be50c2075480c7c5921be3
lgchrist64 <lgchrist64@gmail.com>
2026-02-26 14:29:26 -0600

    Login Page Creation
    -created and styled login page
        -added route in App
    -added wood background to assets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
 import './App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
         {/* Add more routes here as needed */}
       </Routes>
     </BrowserRouter>
